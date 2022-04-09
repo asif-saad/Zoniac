016c4b307e3d586b01ee2975577f515dd18c08d7
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 15:10:37 +0600

    new file random_test

---
+++ app/src/main/java/com/example/data/Random_test.java
@@ -0,0 +1,18 @@
+package com.example.data;
+
+public class Random_test {
+
+    int val;
+
+    public Random_test(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
