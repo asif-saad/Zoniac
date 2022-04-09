e9b0fef965b0649837d3eecff8127fca133cc8f6
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 14:49:21 +0600

    new branch saad updated

---
+++ app/src/main/java/com/example/data/Saad_branch.java
@@ -0,0 +1,21 @@
+package com.example.data;
+
+public class Saad_branch {
+    int value;
+
+    public Saad_branch(int value) {
+        this.value = value;
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+}
