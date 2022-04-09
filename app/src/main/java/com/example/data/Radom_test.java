52a494e160e82b13abe9093415ea336c16e19386
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 15:13:17 +0600

    the same named file from branch saad

---
+++ app/src/main/java/com/example/data/Radom_test.java
@@ -0,0 +1,18 @@
+package com.example.data;
+
+public class Radom_test {
+
+    String name;
+
+    public Radom_test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
