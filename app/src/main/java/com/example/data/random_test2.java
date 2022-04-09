ecb8c3976a5e68fc590e8f1de4f3ff911bb8cb18
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 15:34:44 +0600

    done

--- app/src/main/java/com/example/data/random_test2.java
+++ app/src/main/java/com/example/data/random_test2.java
@@ -3,8 +3,10 @@ package com.example.data;
 public class random_test2 {
 
     String name;
+    Boolean flag;
 
-    public random_test2(String name) {
+    public random_test2(String name, Boolean flag) {
         this.name = name;
+        this.flag = flag;
     }
 }
