72cb902705763ff18a2e58389c440cbe3d693547
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 15:33:52 +0600

    from master branch random_test2 updated

--- app/src/main/java/com/example/data/random_test2.java
+++ app/src/main/java/com/example/data/random_test2.java
@@ -2,8 +2,10 @@ package com.example.data;
 
 public class random_test2 {
     int roll;
+    double result;
 
-    public random_test2(int roll) {
+    public random_test2(int roll, double result) {
         this.roll = roll;
+        this.result = result;
     }
 }
