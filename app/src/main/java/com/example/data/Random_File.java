2e4f50114c5a410fe060db5b98b2363fede0ac6e
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 14:51:29 +0600

    conflict made with master branch

--- app/src/main/java/com/example/data/Random_File.java
+++ app/src/main/java/com/example/data/Random_File.java
@@ -4,10 +4,12 @@ public class Random_File {
 
     int val;
     String name;
+    double result;
 
-    public Random_File(int val, String name) {
+    public Random_File(int val, String name, double result) {
         this.val = val;
         this.name = name;
+        this.result = result;
     }
 
     public int getVal() {
@@ -18,11 +20,19 @@ public class Random_File {
         return name;
     }
 
+    public double getResult() {
+        return result;
+    }
+
     public void setVal(int val) {
         this.val = val;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
     }
 }
