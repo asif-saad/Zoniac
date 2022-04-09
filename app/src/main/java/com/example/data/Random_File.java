931080a6aa68084e324d1fc5d0559ddcf575436a
Prussian Blue <asifsaad730@gmail.com>
2022-04-09 13:31:11 +0600

    done adding temp file java

---
+++ app/src/main/java/com/example/data/Random_File.java
@@ -0,0 +1,28 @@
+package com.example.data;
+
+public class Random_File {
+
+    int val;
+    String name;
+
+    public Random_File(int val, String name) {
+        this.val = val;
+        this.name = name;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
